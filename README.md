b8a7e147e28d7ea6e66a1201d18afb0409e2b1cc
Guilherme Koller <guilhermekoller@pop-os.localdomain>
2022-05-01 22:51:15 -0300

    project setup and sql script file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LibraryDB
+##executar o seguinte comando para criar o banco de dados e inserir os dados carregados do arquivo .csv
+```bash
+	sudo mysql --local-infile=1 -u root -p1 < books.sql
+```
---
+++ books.sql
@@ -0,0 +1,24 @@
+create database library;
+
+use library;
+
+CREATE TABLE books (
+	book_id int PRIMARY KEY, 
+	title varchar(150), 
+	author varchar(100),
+	edition int,
+	year YEAR,
+	publisher varchar(100)
+);
+
+LOAD DATA LOCAL INFILE 'livros-db.csv' INTO TABLE books
+
+FIELDS TERMINATED BY ','
+
+ENCLOSED BY '"'
+
+LINES TERMINATED BY '\n'
+
+IGNORE 1 ROWS;
+
+alter table books add column cover blob;
